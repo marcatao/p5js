d7324f87c8fac5c43af23aa08f9a7174c78c3a91
marcatao <thiagomarcato@gmail.com>
2020-10-07 22:27:09 -0300

    finalizando robo genio

---
+++ README.md
@@ -0,0 +1,9 @@
+#Javascript 1: Criando um robô gênio
+<ul>
+<li>Aprenda os fundamentos do Javascript utilizando o P5</li>
+<li>Saiba como trabalhar com variáveis e imagens</li>
+<li>Descubra como trabalhar com diferentes cenas no P5</li>
+<li>Desenvolva suas próprias funções, tornando seu código elegante e organizado</li>
+<li>Utilize seu conhecimento em Scratch, para aprender outra linguagem de programação</li>
+</ul>
+<a href="https://thiagomarcato1.websiteseguro.com/projects/p501/">Visualizar projeto funcionando</a>
--- RoboP5/src/sketch.js
+++ RoboP5/src/sketch.js
@@ -6,14 +6,17 @@ let imagemBalao;
 //analise e resposta
 let numeroPergunta = 0;
 let caixaResposta;
+let nome ="";
 
 let texto = ["Seja bem vindo Marcatron\n "+
             "Para adivinhar seu nome \n " +
             "responda minhas perguntas \n "+
             "com frases longas \n " +
             "Clique na tela para começar",
-
-            "Qual o seu livro \n favorito?"
+            "Qual o seu livro \n favorito?",
+            "Qual o seu filme \n favorito?",
+            "Qual o sua fruta \n favorita?",
+            "Qual o sua cor   \n favorita?"
             ];
 
 function preload(){
@@ -24,24 +27,57 @@ function preload(){
 
 function setup() {
   createCanvas(500,400)
-  caixaResposta = createInput();
+
 }
 
 function draw() {
   background(backgroundImage);
   image(imagemRobo, 130,220,150,150)  
   image(imagemBalao, 50,40,300,200)
-  textSize(15);
-  textAlign(CENTER);
-  text(texto[numeroPergunta], 190, 80)
+  escreveTexto();
 }
 
+function escreveTexto(){
+  textSize(15);
+  textAlign(CENTER);
+  let txt = texto[numeroPergunta];
+  if(texto.length == numeroPergunta){
+     txt = "Seu nome é: " + nome;
+     caixaResposta.remove();
+  }
+  text(txt , 190, 80)
+
+}
+
+
 function mousePressed(){
-  numeroPergunta++;
-  exibirCaixaResposta();
+  if(numeroPergunta < 1)  exibirCaixaResposta();
+  if(numeroPergunta == 0 ) numeroPergunta++;
+  if(caixaResposta.value().length > 0){
+      numeroPergunta++;
+  }
+}
+
+function keyPressed(){
+  if(keyCode === ENTER){
+    decobrirNome();
+    
+  }
 }
 
 function exibirCaixaResposta(){
-  caixaResposta.position(80,360);
-  caixaResposta.site(450,20);
+  caixaResposta = createInput();
+  caixaResposta.position(50,350);
+  caixaResposta.size(380,30);
+  caixaResposta.elt.placeholder = "Responda aqui e precione a tecla ENTER"
+}
+
+function decobrirNome(){
+  respostaTexto = caixaResposta.value();
+  if(caixaResposta.value().length > 0){
+    nome += respostaTexto[0];
+    console.log(nome);
+    numeroPergunta++;
+    caixaResposta.value('');
+  }
 }
