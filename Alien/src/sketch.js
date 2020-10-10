44418ffbaafa07bcc7ea19b927140fe779b8e93d
marcatao <thiagomarcato@gmail.com>
2020-10-09 22:55:26 -0300

    Ajustes no jogo

--- Alien/src/sketch.js
+++ Alien/src/sketch.js
@@ -1,50 +1,59 @@
 let naveEspacial;
-let imagemAlien; 
+let imagemAlien = new Array(); 
 let imagemMissil;
-
 let posicaoMissil = new Array();
-
 let posicaoNave;
-let posicaoAlien;
+let posicaoAliens = new Array();
 let alienVivo =true;
-
+let pontuacao = 0
 let tamnhoAlien = 5;
 let alienDirecao = true;
 let velocidadeAlien = 5;
+let mySound;
+
 
 function preload(){
+   
+    soundFormats('mp3', 'ogg');
+    mySound = loadSound('../assets/sounds/Trilha.mp3');
+
     naveEspacial = loadImage('../assets/img/Nave.png');
-    imagemAlien = loadImage('../assets/img/Alien1.png');
+    imagemAlien[0] = loadImage('../assets/img/Alien1.png');
+    imagemAlien[1] = loadImage('../assets/img/Alien2.png');
+    imagemAlien[2] = loadImage('../assets/img/Alien3.png');
+
     imagemMissil = loadImage('../assets/img/Missil.png');
-    
- 
+
     posicaoNave = createVector();
     posicaoAlien = createVector();
 
     posicaoNave.x =  mouseX-(naveEspacial.width/2);
     posicaoNave.y = (windowHeight - (naveEspacial.height * 150));
-
-
-
-    posicaoAlien.x = 400;
-    posicaoAlien.y = 300;
 }
 
 function setup() {
-    createCanvas(windowWidth, windowHeight)
+ 
+    createCanvas(windowWidth, windowHeight);
+    mySound.setVolume(0.5);
+    mySound.play();
+
+    criarAlien();
 }
 
 function draw() {
+    movimentarAlien();
     background(100);
-
+    desenharAlien();
     colisaoAlien();
+
     posicaoNave.x =  mouseX-(naveEspacial.width/2);
     image(naveEspacial, posicaoNave.x, posicaoNave.y, setSize(8),setSize(8));
     
-    movimentarAlien();
-    if(alienVivo) image(imagemAlien, posicaoAlien.x, posicaoAlien.y ,setSize(tamnhoAlien),setSize(tamnhoAlien)); 
-   
     desenhaMisseis();
+    
+    fill(250);
+    textSize(25);
+    text("Score: " + pontuacao, windowWidth * 0.01 , windowHeight * 0.05);
 }
 
 function windowResized() {
@@ -56,45 +65,72 @@ function setSize(val){
 }
 
 function mousePressed(){
-    console.log("mousePressed");
-    criarPosicoes();
-
+   criarPosicoes();
 } 
 
 function colisaoAlien(){
-  if(posicaoMissil.length > 0){
+  if(posicaoMissil){
     posicaoMissil.forEach(function (item, i) {
-         if (!(item.x > posicaoAlien.x + setSize(tamnhoAlien) ||
-               item.x + setSize(1) < posicaoAlien.x ||
-               item.y > posicaoAlien.y + setSize(tamnhoAlien) ||
-               item.y + setSize(2) < posicaoAlien.y)) alienVivo = false;
+        if(verificaColisaoAlien(item, i)){
+            console.log('apagar missil:'+i)
+            posicaoMissil.slice(i);
+        } 
     });
   }
 }
 
+function verificaColisaoAlien(item, item_i){
+    if(posicaoAliens){
+        posicaoAliens.forEach(function (alien, i){
+            if (!(item.x > alien[0].x + setSize(tamnhoAlien) ||
+            item.x + setSize(1) < alien[0].x ||
+            item.y > alien[0].y + setSize(tamnhoAlien) ||
+            item.y + setSize(2) < alien[0].y)) {
+                posicaoAliens.splice(i,1);
+                posicaoMissil.splice(item_i,1)
+                pontuacao += 10;
+            }
+        });
+    }
+}
+
+
+function criarAlien(){
+
+    for(let i=0 ; i < 4; i++){
+        let fantasia = Math.floor(Math.random() * Math.floor(3))
+        posicaoAliens.push([createVector((windowHeight*0.15)*i, (windowHeight*0.10)),fantasia]);
+    }
+}
+function desenharAlien(){
+    posicaoAliens.forEach(function (item, i) {
+        image(imagemAlien[item[1]], item[0].x, item[0].y, setSize(tamnhoAlien), setSize(tamnhoAlien));
+    });
+}
+
+
+
 function movimentarAlien(){
-    
-    if((posicaoAlien.x+setSize(tamnhoAlien)) > windowWidth)alienDirecao = false;
-    if(posicaoAlien.x == 0) alienDirecao = true;
-    if(alienDirecao){
-        posicaoAlien.x +=velocidadeAlien;
-    }else{
-        posicaoAlien.x -=velocidadeAlien;
-    }    
+    posicaoAliens.forEach(function (item, i) { 
+          if((item[0].x + setSize(tamnhoAlien)) >= windowWidth) alienDirecao = false;
+          if(item[0].x < 0) alienDirecao = true;
+             if(alienDirecao){
+                item[0].x +=velocidadeAlien;
+             }else{
+               item[0].x -=velocidadeAlien;
+             }    
+    });
     
 }
 
 function criarPosicoes(){
-    console.log('criarMisseis');
     posicaoMissil.push(createVector((((posicaoNave.x+(naveEspacial.width/2))) - (setSize(1)/2)), posicaoNave.y));
- 
 }
 
 function desenhaMisseis(){
     posicaoMissil.forEach(function (item, i) {
         image(imagemMissil, item.x, item.y ,setSize(1),setSize(2)); 
         posicaoMissil[i].y -= 4;
-        if(posicaoMissil[i].y <= 0) posicaoMissil.splice(i,i);
+        if(posicaoMissil[i].y <= 0) posicaoMissil.splice(i,1);
     });
-    console.log(posicaoMissil.length);
 }
